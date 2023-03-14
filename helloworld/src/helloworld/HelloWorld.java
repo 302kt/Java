507eed06015580fd972fbbcfe208cb52120d10da
katarina pereira <kpf.freire@gmail.com>
2023-03-14 11:52:26 -0300

    Projeto hello world

---
+++ helloworld/src/helloworld/HelloWorld.java
@@ -0,0 +1,16 @@
+package helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		int teste = 10;
+		// TODO Auto-generated method stub
+		System.out.print("Hello World!");
+		System.out.print("\tOlá turma 62!");
+		System.out.println("Variável teste: " + teste);
+		
+		
+
+	}
+
+}
