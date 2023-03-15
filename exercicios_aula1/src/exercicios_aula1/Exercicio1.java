f7c4908ac1d70f912d055a7cad10548b36ec2f10
katarina pereira <kpf.freire@gmail.com>
2023-03-15 13:58:24 -0300

    Exercicio2

--- exercicios_aula1/src/exercicios_aula1/Exercicio1.java
+++ exercicios_aula1/src/exercicios_aula1/Exercicio1.java
@@ -1,25 +1,26 @@
 package exercicios_aula1;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Exercicio1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		DecimalFormat df = new DecimalFormat("###,###,##0.00");
 		Scanner leia = new Scanner(System.in);
-		float nota1, nota2, nota3, nota4;
-		String nome_aluno;
-		System.out.println("Digite o nome do aluno");
-		nome_aluno = leia.nextLine();
-		System.out.println("Digite a primeira nota: ");
-		nota1 = leia.nextFloat();
-		System.out.println("Digite a segunda nota: ");
-		nota2 = leia.nextFloat();
-		System.out.println("Digite a terceira nota: ");
-		nota3 = leia.nextFloat();
-		System.out.println("Digite a quarta nota: ");
-		nota4 = leia.nextFloat();
-		System.out.println("Média= " + (nota1 + nota2 + nota3 + nota4) / 4);
+		float salario, abono, novo_salario;
+		
+		System.out.println("Digite seu salário: ");
+		salario = leia.nextFloat();
+		
+		System.out.println("Digite o abono: ");
+		abono = leia.nextFloat();
+		
+		novo_salario = (salario + abono);
+		
+		System.out.println("Seu novo salário é: R$" + df.format(novo_salario));
+		
 	}
 
 }
