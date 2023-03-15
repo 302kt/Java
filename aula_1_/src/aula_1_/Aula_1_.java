26eeb5487a7e5f527fabf6f7c2d747397fb93523
katarina pereira <kpf.freire@gmail.com>
2023-03-15 12:50:39 -0300

    Aula_1_

---
+++ aula_1_/src/aula_1_/Aula_1_.java
@@ -0,0 +1,28 @@
+package aula_1_;
+
+import java.util.Scanner;
+
+public class Aula_1_ {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner leia = new Scanner(System.in);
+				int numero1, numero2;
+		String nome;
+		System.out.println("Digite o seu nome: ");
+		nome = leia.nextLine();
+		System.out.println("Olá," + nome + ".");
+		System.out.println("Digite o primeiro número: ");
+		numero1= leia.nextInt();
+		System.out.println("Digite o segundo número: ");
+		numero2= leia.nextInt();
+		System.out.println("Soma: " + (numero1 + numero2));
+		System.out.println("Diferença: " + (numero1 - numero2));
+		System.out.println("Divisão: " + (numero1 / numero2));
+		System.out.println("Potência: " + (Math.pow(numero1, numero2)));
+		System.out.println("Raiz quadrada " + (Math.sqrt(numero1)));
+		
+	}
+
+}
