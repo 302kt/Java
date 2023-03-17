12feaa5aeda23ce78cbe9c3b97bb75393226c7b2
katarina pereira <kpf.freire@gmail.com>
2023-03-17 18:24:26 -0300

    Exercicios Aula 3

--- aula_3_/src/aula_3_/Exercicios1.java
+++ aula_3_/src/aula_3_/Exercicios1.java
@@ -2,7 +2,7 @@ package aula_3_;
 
 import java.util.Scanner;
 
-public class ExerciciosAula3 {
+public class Exercicios1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
