59ab22ff0e35b95256547182ae27da0e959ae937
katarina pereira <kpf.freire@gmail.com>
2023-03-20 19:55:18 -0300

    Exercicio 3

--- aula_4_/src/aula_4_/Exercicio4.java
+++ aula_4_/src/aula_4_/Exercicio4.java
@@ -1,9 +1,16 @@
 package aula_4_;
 
+import java.util.Scanner;
+
 public class Exercicio4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Scanner leia = new Scanner(System.in);
+		
+		
+		
 
 	}
 
