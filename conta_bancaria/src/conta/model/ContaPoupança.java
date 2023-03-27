20f15db335a7cdd16d0d8aeee243be1b0675bc9c
katarina pereira <kpf.freire@gmail.com>
2023-03-27 15:06:00 -0300

    Conta Corrente e Conta Poupança

--- conta_bancaria/src/conta/model/ContaPoupança.java
+++ conta_bancaria/src/conta/model/ContaPoupança.java
@@ -5,5 +5,20 @@ public class ContaPoupança extends conta {
 	
 	private int aniversario;
 
+	public ContaPoupança(int numero, int agencia, int tipo, String titular, float saldo, int aniversario) {
+		super(numero, agencia, tipo, titular, saldo);
+		this.aniversario = aniversario;
+	}
 
+	public int getAniversario() {
+		return aniversario;
+	}
+
+	public void setAniversario(int aniversario) {
+		this.aniversario = aniversario;
+	}
+
+	public void visualizar() {
+		
+	}
 }
